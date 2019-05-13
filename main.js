7eae3f016d1795dd6e47982c0d7ea47cbd86b472
Billy Chen <billy.chen1206@gmail.com>
2019-05-13 16:56:21 -0400

    another gigabyte commit

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+window.onscroll = function() {yeehaw()}
 
+let navbar = document.getElementById("navbar")
+let sticky = navbar.offsetTop
+
+function yeehaw() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky")
+  }
+}
