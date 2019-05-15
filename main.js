ed6ffac269e15e47cb789bbb75f97cd09f18273c
Billy Chen <billy.chen1206@gmail.com>
2019-05-15 08:17:36 -0400

    finally got animations to work

--- main.js
+++ main.js
@@ -13,3 +13,39 @@ function topFunction() {
   document.body.scrollTop = 0
   document.documentElement.scrollTop = 0
 }
+
+window.addEventListener('scroll', function() {
+  let mulah = document.getElementsByClassName('spooky')
+  if(window.scrollY >= 1) {
+    mulah[0].classList.remove('hidden')
+    mulah[0].classList.add('show')
+  }
+  if(window.scrollY >= 165) {
+    mulah[1].classList.remove('hidden')
+    mulah[1].classList.add('show')
+  }
+  if(window.scrollY >= 330) {
+    mulah[2].classList.remove('hidden')
+    mulah[2].classList.add('show')
+  }
+  if(window.scrollY >= 450) {
+    mulah[3].classList.remove('hidden')
+    mulah[3].classList.add('show')
+  }
+  if(window.scrollY >= 700) {
+    mulah[4].classList.remove('hidden')
+    mulah[4].classList.add('show')
+  }
+  if(window.scrollY >= 1000) {
+    mulah[5].classList.remove('hidden')
+    mulah[5].classList.add('show')
+  }
+  if(window.scrollY >= 1300) {
+    mulah[6].classList.remove('hidden')
+    mulah[6].classList.add('show')
+  }
+  if(window.scrollY >= 1500) {
+    mulah[7].classList.remove('hidden')
+    mulah[7].classList.add('show')
+  }
+})
